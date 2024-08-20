9667ad8f2d7cb4827c0cde96165d5e778c80a228
ahmedrazahuri <ahmedrazaraza316@gmail.com>
2024-08-20 17:58:58 +0500

    coomit

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,15 @@
 // Import necessary polyfills for Angular
 import 'zone.js';  // Included with Angular CLI.
 import 'core-js/es/reflect';
+import 'core-js/es/array';
+import 'core-js/es/array/flat-map';
+import 'core-js/es/array/flat';
+import 'core-js/es/array/includes';
+import 'core-js/es/function';
+import 'core-js/es/number';
+import 'core-js/es/object';
+import 'core-js/es/promise';
+import 'core-js/es/symbol';
+import 'core-js/es/weak-map';
+import 'core-js/es/weak-set';
+
